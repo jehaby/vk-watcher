cabffff2aa796bd8fc38cf425fe6ffe63eb43d35
jehaby <jehaby@ya.ru>
2014-12-01 19:22:47 +0300

    Initial commit

---
+++ app/controllers/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+class LoginController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+
+
+}
